8956ab7c26de907943568b065b60474b8f82edee
zephyr <root@tsuyu.xyz>
2020-08-16 15:36:41 +0900

    handle ctrl+c

--- tcpping/tcpping.go
+++ tcpping/tcpping.go
@@ -1,63 +1,74 @@
 package main
-import(
-	"os"
+
+import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 	"time"
-	"flag"
 )
-var(
-	host=flag.String("h","github.com","ip or domain")
-	port=flag.Int("p",443,"port")
-	count=flag.Int("n",4,"count")
-	timeout=flag.Int("t",1,"timeout of each request")
-	quiet=flag.Bool("q",false,"work in quiet mode")
+
+var (
+	host    = flag.String("h", "github.com", "ip or domain")
+	port    = flag.Int("p", 443, "port")
+	count   = flag.Int("n", 4, "count")
+	timeout = flag.Int("t", 1, "timeout of each request")
+	quiet   = flag.Bool("q", false, "work in quiet mode")
 )
-func main(){
+
+func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
 	flag.Parse()
-	ip:=resolv(*host)
-	target:=fmt.Sprintf("%s:%d",ip,*port)
-	results:=make([]int,*count)
-	fmt.Printf("TCPPING %s (%s):\n",*host,ip)
-	for i:=0;i<*count;i++{
-		results[i]=tcping(target,i+1)
+	ip := resolv(*host)
+	target := fmt.Sprintf("%s:%d", ip, *port)
+	results := make([]int, 0)
+	go func() {
+		<-c
+		analysis(results[:])
+		os.Exit(0)
+	}() //handle interrupt
+	fmt.Printf("TCPPING %s (%s):\n", *host, ip)
+	for i := 1; i <= *count; i++ {
+		results = append(results, tcpping(target, i))
 	}
 	analysis(results[:])
 }
-func resolv(address string)net.IP{
-	ips,err:=net.LookupIP(address)
-	if err!=nil{
-		fmt.Fprintln(os.Stderr,err)
+func resolv(address string) net.IP {
+	ips, err := net.LookupIP(address)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
 	return ips[0]
 }
-func tcping(target string,seq int)int{
-	start:=time.Now()
-	conn,err:=net.DialTimeout("tcp",target,time.Duration(*timeout)*time.Second)
-	end:=time.Now()
-	latency:=int(end.Sub(start).Milliseconds())
+func tcpping(target string, seq int) int {
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", target, time.Duration(*timeout)*time.Second)
+	end := time.Now()
+	latency := int(end.Sub(start).Milliseconds())
 	var status string
-	if err!=nil{
-		status="close"
-	}else{
+	if err != nil {
+		status = "close"
+	} else {
 		conn.Close()
-		status="open"
+		status = "open"
 	}
-	if !*quiet{
-		fmt.Printf("seq%4d: %s[%s] %dms\n",seq,target,status,latency)
+	if !*quiet {
+		fmt.Printf("seq%4d: %s[%s] %dms\n", seq, target, status, latency)
 	}
 	return latency
 }
-func analysis(results []int){
-	min,max,sum:=results[0],results[0],0
-	for _,val:=range results{
+func analysis(results []int) {
+	min, max, sum := results[0], results[0], 0
+	for _, val := range results {
 		sum += val
-		if val < min{
-			min=val
+		if val < min {
+			min = val
 		}
-		if val > max{
-			max=val
+		if val > max {
+			max = val
 		}
 	}
-	fmt.Printf("min/avg/max = %d/%d/%dms\n",min,sum/(*count),max)
+	fmt.Printf("min/avg/max = %d/%d/%dms\n", min, sum/(*count), max)
 }
